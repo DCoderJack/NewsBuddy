3e645b4497e31e485e665c135e78ea056e93dc4c
DCoderJack <codingjack20@gmail.com>
2022-12-23 21:32:12 +0530

    Commiting Final NewBuddy Application

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  LoadingSpinner  from "./LoadingSpinner.gif";
+
+const Spinner = () =>{
+    return (
+      <div className='text-center'>
+        <img src={LoadingSpinner} alt="LoadingSpinner" />
+      </div>
+    )
+  }
+
+export default Spinner
